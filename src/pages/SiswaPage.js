bf00a3ccd289d0cb60076008be3c5def1405921f
deddybrama11 <dedybramayadi@gmail.com>
2021-10-22 00:55:18 +0700

    integrating SiswaPage with  data dummy using localStorage

--- src/pages/SiswaPage.js
+++ src/pages/SiswaPage.js
@@ -2,6 +2,51 @@ import React from "react";
 import Navbar from "../component/Navbar";
 
 function SiswaPage() {
+  const getDataSiswa = (nis) => {
+    const data = JSON.parse(localStorage.getItem("pelanggaran"));
+    const tmp = [];
+    for (let index = 0; index < data.length; index++) {
+      if (data[index].nis === nis) tmp.push(data[index]);
+    }
+    return tmp;
+  };
+  const getTotalPoin = () => {
+    const data = getDataSiswa("E311");
+    var tmp = 0;
+    data.map((object) => {
+      tmp = tmp + getNilaiPelanggaran(object.pelanggaran);
+    });
+    return tmp;
+  };
+
+  const getPelanggaran = (id) => {
+    const dataPelanggaran = JSON.parse(
+      localStorage.getItem("pelanggaran_master")
+    );
+    for (let index = 0; index < dataPelanggaran.length; index++) {
+      if (dataPelanggaran[index].id === parseInt(id)) {
+        // console.log(nis);
+        return dataPelanggaran[index].pelanggaran;
+      } else {
+        continue;
+      }
+    }
+  };
+  const getNilaiPelanggaran = (id) => {
+    const dataPelanggaran = JSON.parse(
+      localStorage.getItem("pelanggaran_master")
+    );
+    for (let index = 0; index < dataPelanggaran.length; index++) {
+      if (dataPelanggaran[index].id === parseInt(id)) {
+        // console.log(nis);
+        return dataPelanggaran[index].nilai;
+        break;
+      } else {
+        continue;
+      }
+    }
+  };
+
   return (
     <Navbar>
       <div class="flex flex-col">
@@ -9,10 +54,12 @@ function SiswaPage() {
           <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
             <div class="flex justify-center text-3xl text-center font-extrabold mx-auto py-6 px-4 sm:px-6 lg:px-8">
               <div className="p-8 inline-block border-solid border-4">
-                0/100
+                {getTotalPoin()}/100
               </div>
             </div>
-            <div className="my-2 ml-2 text-md text-gray-800">Riwayat dosamu</div>
+            <div className="my-2 ml-2 text-md text-gray-800">
+              Riwayat dosamu
+            </div>
 
             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
               <table class="min-w-full divide-y divide-gray-200">
@@ -45,50 +92,34 @@ function SiswaPage() {
                   </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200">
-                  <tr>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      <div class="flex items-center">
-                        <div class="text-sm font-medium text-gray-700">
-                          14 February 2021
-                        </div>
-                      </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      <div class="text-sm font-medium text-gray-700">
-                        Main hp dikelas
-                      </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      <div class="text-sm font-medium text-gray-1000">
-                        Bu Hartini.spd
-                      </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      <div class="text-sm font-medium text-gray-700">100</div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      <div class="flex items-center">
-                        <div class="text-sm font-medium text-gray-700">
-                          14 February 2021
-                        </div>
-                      </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      <div class="text-sm font-medium text-gray-700">
-                        Main hp dikelas
-                      </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      <div class="text-sm font-medium text-gray-1000">
-                        Bu Hartini.spd
-                      </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      <div class="text-sm font-medium text-gray-900">100</div>
-                    </td>
-                  </tr>
+                  {getDataSiswa("E311").map((object) => {
+                    return (
+                      <tr>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                          <div class="flex items-center">
+                            <div class="text-sm font-medium text-gray-700">
+                              {object.tanggal}
+                            </div>
+                          </div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                          <div class="text-sm font-medium text-gray-700">
+                            {getPelanggaran(object.pelanggaran)}
+                          </div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                          <div class="text-sm font-medium text-gray-1000">
+                            Bu Hartini.spd
+                          </div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          <div class="text-sm font-medium text-gray-700">
+                            {getNilaiPelanggaran(object.pelanggaran)}
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  })}
                 </tbody>
               </table>
             </div>
