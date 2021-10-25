aa13a7cd1f1ab5bb55b3983d3d3cf3dfc780ca89
deddybrama11 <dedybramayadi@gmail.com>
2021-10-25 10:43:07 +0700

    changed image profile and build

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import profile from "../assets/image/profile.png"
 function Navbar(props) {
   const [show, setShow] = useState(false);
   const [showMobile, setShowMobile] = useState(false);
@@ -101,7 +101,7 @@ function Navbar(props) {
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src={profile}
                         alt=""
                       />
                     </button>
@@ -238,7 +238,7 @@ function Navbar(props) {
                 <div className="flex-shrink-0">
                   <img
                     className="h-10 w-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    src={profile}
                     alt=""
                   />
                 </div>
